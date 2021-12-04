3e65274055e932834984bc84a2df5031423edea5
Yanuar Aditia <yanuaraditia@outlook.com>
2021-12-04 13:24:20 +0700

    first commit

--- plugins/client.js
+++ plugins/client.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  "your-supabase-api-url",
-  "your-supabase-public-api-key"
+  "https://wafybebqjsjhlpouawek.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODU5ODE2NCwiZXhwIjoxOTU0MTc0MTY0fQ.rKQNpvnK9ab2zTSNDm3KNklp68py60ezbQF1hynakww"
 )
 
 export default (_, inject) => {
